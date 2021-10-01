d768068b96aa83e50b995976bd1386da11ba13aa
Seth Bashford <seth.bashford@protonmail.com>
2021-10-01 13:20:23 -0500

    shapes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <SDL.h>
 #include <SDL_events.h>
+#include <SDL_render.h>
 #include <SDL_stdinc.h>
 
 #include <iostream>
 #include <map>
+
+constexpr auto screenWidth{640};
+constexpr auto screenHeight{480};
 
 enum class Color { red, blue, green, white };
 
@@ -13,14 +17,22 @@ int main() {
               << '\n';
     return 1;
   }
-  auto *window =
-      SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
-                       SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+  auto *window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+                                  SDL_WINDOWPOS_UNDEFINED, screenWidth,
+                                  screenHeight, SDL_WINDOW_SHOWN);
   if (window == nullptr) {
     std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError()
               << '\n';
     return 1;
   }
+
+  auto *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (renderer == nullptr) {
+    std::cerr << "Renderer could not be created! SDL Error: " << SDL_GetError()
+              << '\n';
+    return 1;
+  }
+
   auto *screenSurface = SDL_GetWindowSurface(window);
   std::map<Color, Uint32> colorCodes;
   colorCodes[Color::red] = SDL_MapRGB(screenSurface->format, 0xFF, 0, 0);
@@ -50,10 +62,24 @@ int main() {
       default:
         break;
       }
-    SDL_FillRect(screenSurface, nullptr, colorCode);
-    SDL_UpdateWindowSurface(window);
-  }
+    // Clear screen
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(renderer);
+    SDL_Rect fillRect = {screenWidth / 4, screenHeight / 4, screenWidth / 2,
+                         screenHeight / 2};
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+    SDL_RenderFillRect(renderer, &fillRect);
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
+    SDL_RenderDrawLine(renderer, 0, screenHeight / 2, screenWidth,
+                       screenHeight / 2);
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0x00, 0xFF);
+    for (int i = 0; i < screenHeight; i += 4) {
+      SDL_RenderDrawPoint(renderer, screenWidth / 2, i);
+    }
 
+    SDL_RenderPresent(renderer);
+  }
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
 }
