003959be19e4797e89f5ad0ec9e4dbb5d634893b
Seth Bashford <seth.bashford@protonmail.com>
2021-10-01 15:27:30 -0500

    refactor

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "SDL_surface.h"
 #include <SDL.h>
 #include <SDL_events.h>
 #include <SDL_hints.h>
@@ -7,68 +8,118 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <map>
+#include <sstream>
+#include <stdexcept>
 #include <string>
 
+namespace sbash64::game {
 constexpr auto screenWidth{640};
 constexpr auto screenHeight{480};
 
 enum class Color { red, blue, green, white };
 
-int main(int argc, char *argv[]) {
-  if (argc < 2)
-    return EXIT_FAILURE;
-
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
-    std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
-              << '\n';
-    return EXIT_FAILURE;
+namespace sdl_wrappers {
+struct Init {
+  Init() {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+      std::stringstream stream;
+      stream << "SDL could not initialize! SDL_Error: " << SDL_GetError();
+      throw std::runtime_error{stream.str()};
+    }
   }
+
+  ~Init() { SDL_Quit(); }
+};
+
+struct Window {
+  Window()
+      : window{SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+                                SDL_WINDOWPOS_UNDEFINED, screenWidth,
+                                screenHeight, SDL_WINDOW_SHOWN)} {
+    if (window == nullptr) {
+      std::stringstream stream;
+      stream << "Window could not be created! SDL_Error: " << SDL_GetError();
+      throw std::runtime_error{stream.str()};
+    }
+  }
+
+  ~Window() { SDL_DestroyWindow(window); }
+
+  SDL_Window *window;
+};
+
+struct Renderer {
+  explicit Renderer(SDL_Window *window)
+      : renderer{SDL_CreateRenderer(
+            window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)} {
+    if (renderer == nullptr) {
+      std::stringstream stream;
+      stream << "Renderer could not be created! SDL_Error: " << SDL_GetError();
+      throw std::runtime_error{stream.str()};
+    }
+  }
+
+  ~Renderer() { SDL_DestroyRenderer(renderer); }
+
+  SDL_Renderer *renderer;
+};
+
+struct Texture {
+  Texture(SDL_Renderer *renderer, SDL_Surface *surface)
+      : texture{SDL_CreateTextureFromSurface(renderer, surface)} {
+    if (texture == nullptr) {
+      std::stringstream stream;
+      stream << "Unable to create texture! SDL_Error: " << SDL_GetError();
+      throw std::runtime_error{stream.str()};
+    }
+  }
+
+  ~Texture() { SDL_DestroyTexture(texture); }
+
+  SDL_Texture *texture;
+};
+
+struct ImageSurface {
+  ImageSurface(const std::string &imagePath)
+      : surface{IMG_Load(imagePath.c_str())} {
+    if (surface == nullptr) {
+      std::stringstream stream;
+      stream << "Unable to load image " << imagePath
+             << "! SDL_Error: " << SDL_GetError();
+      throw std::runtime_error{stream.str()};
+    }
+  }
+
+  ~ImageSurface() {}
+
+  SDL_Surface *surface;
+};
+} // namespace sdl_wrappers
+
+auto run(const std::string &imagePath) -> int {
+  sdl_wrappers::Init scopedInitialization;
+  sdl_wrappers::Window windowWrapper;
+  sdl_wrappers::Renderer rendererWrapper{windowWrapper.window};
   SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
-  auto *window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
-                                  SDL_WINDOWPOS_UNDEFINED, screenWidth,
-                                  screenHeight, SDL_WINDOW_SHOWN);
-  if (window == nullptr) {
-    std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError()
-              << '\n';
-    return EXIT_FAILURE;
-  }
 
-  auto *renderer = SDL_CreateRenderer(
-      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-  if (renderer == nullptr) {
-    std::cerr << "Renderer could not be created! SDL Error: " << SDL_GetError()
-              << '\n';
-    return EXIT_FAILURE;
-  }
-  SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-  auto imageFlags = IMG_INIT_PNG;
+  SDL_SetRenderDrawColor(rendererWrapper.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+  constexpr auto imageFlags{IMG_INIT_PNG};
   if (!(IMG_Init(imageFlags) & imageFlags)) {
-    std::cerr << "SDL_image could not initialize! SDL_image Error: "
-              << IMG_GetError() << '\n';
-    return 1;
+    std::stringstream stream;
+    stream << "SDL_image could not initialize! SDL_image Error: "
+           << IMG_GetError();
+    throw std::runtime_error{stream.str()};
   }
 
-  std::string path{argv[1]};
-  auto *surface = IMG_Load(path.c_str());
-  if (surface == nullptr) {
-    std::cerr << "Unable to load image %s! SDL_image Error: " << path
-              << IMG_GetError() << '\n';
-    return EXIT_FAILURE;
-  }
-  SDL_SetColorKey(surface, SDL_TRUE,
-                  SDL_MapRGB(surface->format, 0, 0xFF, 0xFF));
+  sdl_wrappers::ImageSurface imageSurfaceWrapper{imagePath};
+  SDL_SetColorKey(
+      imageSurfaceWrapper.surface, SDL_TRUE,
+      SDL_MapRGB(imageSurfaceWrapper.surface->format, 0, 0xFF, 0xFF));
 
-  auto *texture = SDL_CreateTextureFromSurface(renderer, surface);
-  if (texture == nullptr) {
-    std::cerr << "Unable to create texture from %s! SDL Error: " << path
-              << SDL_GetError() << '\n';
-    return EXIT_FAILURE;
-  }
-  const auto imageWidth{surface->w};
-  const auto imageHeight{surface->h};
-
-  SDL_FreeSurface(surface);
+  sdl_wrappers::Texture textureWrapper{rendererWrapper.renderer,
+                                       imageSurfaceWrapper.surface};
+  const auto imageWidth{imageSurfaceWrapper.surface->w};
+  const auto imageHeight{imageSurfaceWrapper.surface->h};
 
   auto running{true};
   auto x{15};
@@ -94,15 +145,24 @@ int main(int argc, char *argv[]) {
       default:
         break;
       }
-    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(rendererWrapper.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(rendererWrapper.renderer);
     SDL_Rect renderQuad = {x, y, imageWidth, imageHeight};
-    SDL_RenderCopyEx(renderer, texture, nullptr, &renderQuad, 0, nullptr,
-                     SDL_FLIP_NONE);
-    SDL_RenderPresent(renderer);
+    SDL_RenderCopyEx(rendererWrapper.renderer, textureWrapper.texture, nullptr,
+                     &renderQuad, 0, nullptr, SDL_FLIP_NONE);
+    SDL_RenderPresent(rendererWrapper.renderer);
   }
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  return EXIT_SUCCESS;
+}
+} // namespace sbash64::game
+
+int main(int argc, char *argv[]) {
+  if (argc < 2)
+    return EXIT_FAILURE;
+  try {
+    return sbash64::game::run(argv[1]);
+  } catch (const std::runtime_error &e) {
+    std::cerr << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
 }
