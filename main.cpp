80c193212b8dc82df917937b7e5296f71c2076e3
Seth Bashford <seth.bashford@protonmail.com>
2021-10-01 15:33:33 -0500

    more refactor

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <string_view>
 
 namespace sbash64::game {
 constexpr auto screenWidth{640};
@@ -18,14 +19,17 @@ constexpr auto screenHeight{480};
 
 enum class Color { red, blue, green, white };
 
+static void throwRuntimeError(std::string_view message) {
+  std::stringstream stream;
+  stream << message << " SDL_Error: " << SDL_GetError();
+  throw std::runtime_error{stream.str()};
+}
+
 namespace sdl_wrappers {
 struct Init {
   Init() {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
-      std::stringstream stream;
-      stream << "SDL could not initialize! SDL_Error: " << SDL_GetError();
-      throw std::runtime_error{stream.str()};
-    }
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+      throwRuntimeError("SDL could not initialize!");
   }
 
   ~Init() { SDL_Quit(); }
@@ -36,11 +40,8 @@ struct Window {
       : window{SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
                                 SDL_WINDOWPOS_UNDEFINED, screenWidth,
                                 screenHeight, SDL_WINDOW_SHOWN)} {
-    if (window == nullptr) {
-      std::stringstream stream;
-      stream << "Window could not be created! SDL_Error: " << SDL_GetError();
-      throw std::runtime_error{stream.str()};
-    }
+    if (window == nullptr)
+      throwRuntimeError("Window could not be created!");
   }
 
   ~Window() { SDL_DestroyWindow(window); }
@@ -52,11 +53,8 @@ struct Renderer {
   explicit Renderer(SDL_Window *window)
       : renderer{SDL_CreateRenderer(
             window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)} {
-    if (renderer == nullptr) {
-      std::stringstream stream;
-      stream << "Renderer could not be created! SDL_Error: " << SDL_GetError();
-      throw std::runtime_error{stream.str()};
-    }
+    if (renderer == nullptr)
+      throwRuntimeError("Renderer could not be created!");
   }
 
   ~Renderer() { SDL_DestroyRenderer(renderer); }
@@ -67,11 +65,8 @@ struct Renderer {
 struct Texture {
   Texture(SDL_Renderer *renderer, SDL_Surface *surface)
       : texture{SDL_CreateTextureFromSurface(renderer, surface)} {
-    if (texture == nullptr) {
-      std::stringstream stream;
-      stream << "Unable to create texture! SDL_Error: " << SDL_GetError();
-      throw std::runtime_error{stream.str()};
-    }
+    if (texture == nullptr)
+      throwRuntimeError("Unable to create texture!");
   }
 
   ~Texture() { SDL_DestroyTexture(texture); }
@@ -80,13 +75,12 @@ struct Texture {
 };
 
 struct ImageSurface {
-  ImageSurface(const std::string &imagePath)
+  explicit ImageSurface(const std::string &imagePath)
       : surface{IMG_Load(imagePath.c_str())} {
     if (surface == nullptr) {
       std::stringstream stream;
-      stream << "Unable to load image " << imagePath
-             << "! SDL_Error: " << SDL_GetError();
-      throw std::runtime_error{stream.str()};
+      stream << "Unable to load image " << imagePath << "!";
+      throwRuntimeError(stream.str());
     }
   }
 
